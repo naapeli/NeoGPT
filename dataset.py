5f74b0ad09de232031a9767481c8c8ae618cbebd
naapeli <130310206+naapeli@users.noreply.github.com>
2025-03-22 13:44:20 +0200

    Complete training loop and validation methods

--- dataset.py
+++ dataset.py
@@ -1,5 +1,105 @@
 import torch
 import tiktoken
+import multiprocessing as mp
+import numpy as np
+from datasets import load_dataset
+from tqdm import tqdm
+import os
+
+
+def save_fineweb():
+    local_dir = "edu_fineweb10B"
+    remote_name = "sample-10BT"
+    shard_size = int(1e8)
+
+    DATA_CACHE_DIR = os.path.join(os.path.dirname(__file__), local_dir)
+    os.makedirs(DATA_CACHE_DIR, exist_ok=True)
+
+    fw = load_dataset("HuggingFaceFW/fineweb-edu", name=remote_name, split="train")
+
+    enc = tiktoken.get_encoding("gpt2")
+    eot = enc._special_tokens["<|endoftext|>"]
+    def tokenize(doc):
+        tokens = [eot]
+        tokens.extend(enc.encode_ordinary(doc["text"]))
+        tokens_np = np.array(tokens)
+        assert (0 <= tokens_np).all() and (tokens_np < 2**16).all(), "token dictionary too large for uint16"
+        tokens_np_uint16 = tokens_np.astype(np.uint16)
+        return tokens_np_uint16
+
+    def write_datafile(filename, tokens_np):
+        np.save(filename, tokens_np)
+
+    nprocs = max(1, os.cpu_count() - 1)
+    with mp.Pool(nprocs) as pool:
+        shard_index = 0
+        all_tokens_np = np.empty((shard_size,), dtype=np.uint16)
+        token_count = 0
+        progress_bar = None
+        for tokens in pool.imap(tokenize, fw, chunksize=16):
+
+            if token_count + len(tokens) < shard_size:
+                all_tokens_np[token_count:token_count+len(tokens)] = tokens
+                token_count += len(tokens)
+                if progress_bar is None:
+                    progress_bar = tqdm(total=shard_size, unit="tokens", desc=f"Shard {shard_index}")
+                progress_bar.update(len(tokens))
+            else:
+                split = "val" if shard_index == 0 else "train"
+                filename = os.path.join(DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}")
+                remainder = shard_size - token_count
+                progress_bar.update(remainder)
+                all_tokens_np[token_count:token_count+remainder] = tokens[:remainder]
+                write_datafile(filename, all_tokens_np)
+                shard_index += 1
+                progress_bar = None
+                all_tokens_np[0:len(tokens)-remainder] = tokens[remainder:]
+                token_count = len(tokens)-remainder
+                break
+
+        if token_count != 0:
+            split = "val" if shard_index == 0 else "train"
+            filename = os.path.join(DATA_CACHE_DIR, f"edufineweb_{split}_{shard_index:06d}")
+            write_datafile(filename, all_tokens_np[:token_count])
+
+class FineWebLoader:
+    def __init__(self, B, T, process_rank, world_size, split):
+        self.B = B
+        self.T = T
+        self.process_rank = process_rank
+        self.world_size = world_size
+        assert split in {"train", "val"}
+
+        data_root = "edu_fineweb10B"
+        shards = os.listdir(data_root)
+        shards = [s for s in shards if split in s]
+        shards = sorted(shards)
+        shards = [os.path.join(data_root, s) for s in shards]
+        self.shards = shards
+        assert len(shards) > 0, f"no shards found for split {split}"
+        self.reset()
+
+    def reset(self):
+        self.current_shard = 0
+        self.tokens = self.load_tokens(self.shards[self.current_shard])
+        self.current_position = self.B * self.T * self.process_rank
+    
+    def load_tokens(self, filename):
+        tokens = np.load(filename)
+        tokens = tokens.astype(np.int32)
+        tokens = torch.tensor(tokens, dtype=torch.long)
+        return tokens
+
+    def next_batch(self):
+        tokens = self.tokens[self.current_position:self.current_position + self.B * self.T + 1]
+        x = tokens[:-1].view(self.B, self.T)
+        y = tokens[1:].view(self.B, self.T)
+        self.current_position += self.B * self.T * self.world_size
+        if self.current_position + self.B * self.T * self.world_size + 1 > len(self.tokens):
+            self.current_shard = (self.current_shard + 1) % len(self.shards)
+            self.tokens = self.load_tokens(self.shards[self.current_shard])
+            self.current_position = self.B * self.T * self.process_rank
+        return x, y
 
 
 def get_shakespeare(B, T, device=torch.device("cpu")):
@@ -40,20 +140,6 @@ class ShakespeareLoader:
             self.current_position = self.B * self.T * self.process_rank
         return x, y
 
-# def ShakespeareLoader(B, T):
-#     with open("shakespeare.txt") as f:
-#         text = f.read()
-#     tokenizer = tiktoken.get_encoding("gpt2")
-#     tokens = tokenizer.encode(text)
-#     tokens = torch.tensor(tokens)
-#     current_position = 0
 
-#     while True:
-#         batch_tokens = tokens[current_position:current_position + B * T + 1]
-#         x = batch_tokens[:-1].view(B, T)
-#         y = batch_tokens[1:].view(B, T)
-#         current_position += B * T
-
-#         if current_position + B * T > len(tokens):
-#             current_position = 0
-#         yield x, y
+if __name__ == "__main__":
+    save_fineweb()
--- train_model.py
+++ train_model.py
@@ -1,10 +1,14 @@
-from dataset import ShakespeareLoader
+from dataset import ShakespeareLoader, FineWebLoader
 from GPT2 import GPT2, Config
 from optimizer import CosineAnnealingWithWarmup, configure_optimizer
+from hellaswag import iterate_examples, render_example, get_most_likely_row
 
 import torch
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
 from torch.distributed import init_process_group, destroy_process_group, all_reduce, ReduceOp
 from torch.nn.parallel import DistributedDataParallel
+import tiktoken
 
 from time import perf_counter
 import os
@@ -12,8 +16,9 @@ import os
 # ============ DEVICE ============
 ddp = int(os.environ.get("RANK", -1)) != -1
 if ddp:
+    # torchrun --standalone --nproc_per_node=1 train_model.py
     assert torch.cuda.is_available()
-    init_process_group(backend="gloo")  # backend="nccl"  # gloo for amd gpu or cpu and nccl for nvidia gpu
+    init_process_group(backend="nccl")  # backend="nccl"  # gloo for amd gpu or cpu and nccl for nvidia gpu
     ddp_rank = int(os.environ["RANK"])
     ddp_local_rank = int(os.environ["LOCAL_RANK"])
     ddp_world_size = int(os.environ["WORLD_SIZE"])
@@ -21,12 +26,14 @@ if ddp:
     torch.cuda.set_device(device)
     master_process = ddp_rank == 0
 else:
+    # python train_model.py
     ddp_rank = 0
     ddp_local_rank = 0
     ddp_world_size = 1
     master_process = True
-    dev = "cuda" if torch.cuda.is_available() else "cpu"
-    device = torch.device(dev)
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
+device_type = "cuda" if device.startswith("cuda") else "cpu"
+device = torch.device(device)
 
 # ============ DATALOADER ============
 total_batch_size = 256  # 524288
@@ -34,8 +41,11 @@ B = 4  # 64
 T = 32  # 2048
 assert total_batch_size % (B * T * ddp_world_size) == 0
 grad_accum_steps = total_batch_size // (B * T * ddp_world_size)
-print(f"Accumulation steps in training: {grad_accum_steps}")
-dataloader = ShakespeareLoader(B, T, ddp_local_rank, ddp_world_size)
+if master_process: print(f"Accumulation steps in training: {grad_accum_steps}")
+# trainloader = ShakespeareLoader(B, T, ddp_local_rank, ddp_world_size)
+# validationloader = ShakespeareLoader(B, T, ddp_local_rank, ddp_world_size)
+trainloader = FineWebLoader(B, T, ddp_local_rank, ddp_world_size, "train")
+validationloader = FineWebLoader(B, T, ddp_local_rank, ddp_world_size, "val")
 
 # ============ SEED AND PRECISION ============
 torch.manual_seed(0)
@@ -52,25 +62,131 @@ if ddp: model = DistributedDataParallel(model, device_ids=[ddp_local_rank])
 # ============ OPTIMIZER ============
 max_lr = 6e-4
 min_lr = 0.1 * max_lr
-warmup_steps = 10
-max_steps = 50
+warmup_steps = 10  # 200
+max_steps = 50  # 19073
 lr_scheduler = CosineAnnealingWithWarmup(min_lr, max_lr, warmup_steps, max_steps)
 optimizer = configure_optimizer(model.named_parameters(), weight_decay=0.1, learning_rate=6e-4)
 
+# ============ LOGGING ============
+log_dir = "log"
+os.makedirs(log_dir, exist_ok=True)
+log_file = os.path.join(log_dir, f"log.txt")
+with open(log_file, "w") as f:
+    pass
+if master_process:
+    writer = SummaryWriter(log_dir="runs/training")
+
+def model_validation(step):
+    validationloader.reset()
+    with torch.no_grad():
+        val_loss_accum = 0.0
+        val_loss_steps = 20
+        for _ in range(val_loss_steps):
+            x, y = validationloader.next_batch()
+            x, y = x.to(device), y.to(device)
+            with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+                _, loss = model(x, y)
+            loss = loss / val_loss_steps
+            val_loss_accum += loss.detach()
+    if ddp:
+        all_reduce(val_loss_accum, op=ReduceOp.AVG)
+    if master_process:
+        print(f"validation loss: {val_loss_accum.item():.4f}")
+        with open(log_file, "a") as f:
+            f.write(f"{step} val {val_loss_accum.item():.4f}\n")
+        writer.add_scalar("Loss/val", val_loss_accum.item(), step)
+            
+
+def save_model():
+    checkpoint_path = os.path.join(log_dir, f"model_{step:05d}.pt")
+    checkpoint = {
+        'model': model.module.state_dict() if ddp else model.state_dict(),
+        'config': model.module.config if ddp else model.config,
+        'step': step
+    }
+    torch.save(checkpoint, checkpoint_path)
+
+def evaluate_hella_swag(step):
+    num_correct_norm = 0
+    num_total = 0
+    for i, example in enumerate(iterate_examples("val")):
+        if i % ddp_world_size != ddp_rank:
+            continue
+        _, tokens, mask, label = render_example(example)
+        tokens = tokens.to(device)
+        mask = mask.to(device)
+        with torch.no_grad():
+            # with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+            #     logits, _ = model(tokens)
+            logits, _ = model(tokens)
+            pred_norm, _, _ = get_most_likely_row(tokens, mask, logits)
+        num_total += 1
+        num_correct_norm += int(pred_norm == label)
+    if ddp:
+        num_total = torch.tensor(num_total, dtype=torch.long, device=device)
+        num_correct_norm = torch.tensor(num_correct_norm, dtype=torch.long, device=device)
+        all_reduce(num_total, op=ReduceOp.SUM)
+        all_reduce(num_correct_norm, op=ReduceOp.SUM)
+        num_total = num_total.item()
+        num_correct_norm = num_correct_norm.item()
+    acc_norm = num_correct_norm / num_total
+    if master_process:
+        print(f"HellaSwag accuracy: {num_correct_norm}/{num_total}={acc_norm:.4f}")
+        with open(log_file, "a") as f:
+            f.write(f"{step} hella {acc_norm:.4f}\n")
+        writer.add_scalar("HellaSwag accuracy", acc_norm, step)
+
+def generate_text():
+    num_return_sequences = 4
+    max_length = 32
+    tokenizer = tiktoken.get_encoding("gpt2")
+    tokens = tokenizer.encode("Hello, I'm a language model,")
+    tokens = torch.tensor(tokens, dtype=torch.long)
+    tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)
+    xgen = tokens.to(device)
+    sample_rng = torch.Generator(device=device)
+    sample_rng.manual_seed(42 + ddp_rank)
+    while xgen.size(1) < max_length:
+        with torch.no_grad():
+            # with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+            #     logits, _ = model(xgen)
+            logits, _ = model(xgen)
+            logits = logits[:, -1, :]
+            probs = F.softmax(logits, dim=-1)
+            topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)
+            ix = torch.multinomial(topk_probs, 1, generator=sample_rng)
+            xcol = torch.gather(topk_indices, -1, ix)
+            xgen = torch.cat((xgen, xcol), dim=1)
+    for i in range(num_return_sequences):
+        tokens = xgen[i, :max_length].tolist()
+        decoded = tokenizer.decode(tokens)
+        print(f"rank {ddp_rank} sample {i}: {decoded}")
+
 # ============ TRAINING LOOP ============
-for step in range(1, 50 + 1):
+for step in range(1, max_steps + 1):
     t0 = perf_counter()
+
+    # validation
+    if step % 250 or step == max_steps:
+        model.eval()
+        model_validation(step)
+        evaluate_hella_swag(step)
+        generate_text()
+        model.train()
+    if master_process and (step % 5000 == 0 or step == max_steps):
+        save_model()
+
     total_loss = 0
     for sub_step in range(grad_accum_steps):
-        x, y = dataloader.next_batch()
+        x, y = trainloader.next_batch()
         x, y = x.to(device), y.to(device)
+        if ddp: model.require_backward_grad_sync = sub_step == grad_accum_steps - 1  # only sync the gradients on the last iteration of the accumulation loop
         optimizer.zero_grad()
-        # with torch.autocast(device_type=dev, dtype=torch.bfloat16):
+        # with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
         #     logits, loss = model(x, targets=y)
         logits, loss = model(x, targets=y)
         loss = loss / grad_accum_steps
-        total_loss += loss.item()
-        if ddp: model.require_backward_grad_sync = sub_step == grad_accum_steps - 1  # only sync the gradients on the last iteration of the accumulation loop
+        total_loss += loss.detach()
         loss.backward()
 
     if ddp: all_reduce(total_loss, op=ReduceOp.AVG)
@@ -81,10 +197,15 @@ for step in range(1, 50 + 1):
         param_group["lr"] = lr
 
     optimizer.step()
-    # torch.cuda.synchronize()
+    if device_type == "cuda": torch.cuda.synchronize()
     t1 = perf_counter()
     dt = t1 - t0
     tokens_processed = grad_accum_steps * B * T * ddp_world_size
-    if master_process: print(f"Step {step:5d} | loss: {total_loss:.6f} | norm of gradients: {norm:.2f} | lr: {lr:.3e} | tokens / sec: {tokens_processed / dt:.2f} | dt: {1000 * dt:.2f} ms")
+    if master_process:
+        print(f"Step {step:5d} | loss: {total_loss.item():.6f} | norm of gradients: {norm:.2f} | lr: {lr:.3e} | tokens / sec: {tokens_processed / dt:.2f} | dt: {1000 * dt:.2f} ms")
+        with open(log_file, "a") as f:
+            f.write(f"{step} train {total_loss.item():.6f}\n")
+        writer.add_scalar("Loss/train", total_loss.item(), step)
 
+if master_process: writer.close()
 if ddp: destroy_process_group()
